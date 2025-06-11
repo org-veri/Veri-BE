0de115e6b8628896f369f6f21fa4af37df5a9461
Jeongmo Seo <wjdah1402@gmail.com>
2025-06-11 14:46:23 +0900

    chore. 프로젝트 초기 세팅

---
+++ src/main/java/org/goorm/veri/veribe/VeriBeApplication.java
@@ -0,0 +1,13 @@
+package org.goorm.veri.veribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeriBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeriBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/goorm/veri/veribe/VeriBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.goorm.veri.veribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeriBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
