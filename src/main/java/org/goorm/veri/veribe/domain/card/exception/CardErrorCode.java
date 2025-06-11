6152ce1d5affde32e5bbae1cc8b2df930f2252cc
Soap <miensop51@gmail.com>
2025-06-12 00:38:06 +0900

    feat. 카드 에러 클래스

---
+++ src/main/java/org/goorm/veri/veribe/domain/card/exception/CardErrorCode.java
@@ -0,0 +1,28 @@
+package org.goorm.veri.veribe.domain.card.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.namul.api.payload.code.BaseErrorCode;
+import org.namul.api.payload.code.dto.ErrorReasonDTO;
+import org.namul.api.payload.code.dto.supports.DefaultResponseErrorReasonDTO;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum CardErrorCode implements BaseErrorCode {
+    NOT_FOUND(HttpStatus.NOT_FOUND, "CARD404", "카드를 찾을 수 없습니다."),
+    UNSUPPORTED_IMAGE_TYPE(HttpStatus.BAD_REQUEST, "CARD4001", "지원하지 않는 파일 형식입니다. 이미지만 업로드할 수 있습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ErrorReasonDTO getReason() {
+        return DefaultResponseErrorReasonDTO.builder()
+                .httpStatus(this.httpStatus)
+                .code(this.code)
+                .message(this.message)
+                .build();
+    }
+}
