8c3860f13b2cb38a195da1737757bc289fb9cfe8
Jinyoung <glitt5384@naver.com>
2025-06-15 17:13:17 +0900

    feat. 이미지 추출 메서드 추가 (interface)

---
+++ src/main/java/org/goorm/veri/veribe/domain/image/service/ImageCommandService.java
@@ -0,0 +1,8 @@
+package org.goorm.veri.veribe.domain.image.service;
+
+import org.goorm.veri.veribe.domain.image.exception.ImageException;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageCommandService {
+    String extractTextFromBook(MultipartFile file) throws Exception;
+}
