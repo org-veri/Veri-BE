f9a524059a59c5a797270667c404061b49b733d7
Jinyoung <glitt5384@naver.com>
2025-06-19 21:36:24 +0900

    feat. 이미지 인코딩 에러 코드 추가

--- src/main/java/org/goorm/veri/veribe/domain/image/exception/ImageErrorCode.java
+++ src/main/java/org/goorm/veri/veribe/domain/image/exception/ImageErrorCode.java
@@ -12,7 +12,8 @@ import org.springframework.http.HttpStatus;
 public enum ImageErrorCode implements BaseErrorCode {
     NOT_FOUND(HttpStatus.NOT_FOUND, "IMG404", "해당 이미지를 찾을 수 없습니다."),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "IMG400", "잘못된 요청입니다. 요청을 확인해주세요."),
-    FORBIDDEN(HttpStatus.FORBIDDEN, "IMG401", "해당 이미지에 대한 권한이 없습니다.");
+    FORBIDDEN(HttpStatus.FORBIDDEN, "IMG401", "해당 이미지에 대한 권한이 없습니다."),
+    ENCODING_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "IMG500_1", "이미지 인코딩에 실패했습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
