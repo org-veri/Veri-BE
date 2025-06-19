6d834bfc462246558961c6d089060e9dc644bd8b
Jinyoung <glitt5384@naver.com>
2025-06-19 21:35:19 +0900

    feat. 사용자별 디렉토리 생성

--- src/main/java/org/goorm/veri/veribe/domain/image/service/ImageCommandService.java
+++ src/main/java/org/goorm/veri/veribe/domain/image/service/ImageCommandService.java
@@ -5,4 +5,5 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageCommandService {
     String extractTextFromBook(MultipartFile file) throws Exception;
+    String makeUserDirectory(String email);
 }
