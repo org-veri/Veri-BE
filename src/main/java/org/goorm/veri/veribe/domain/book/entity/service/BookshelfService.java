53344a192813b09a5fb6ca26d11efcef9913fc36
ojy <ojy654@naver.com>
2025-06-20 16:07:33 +0900

    feat. MemberBook 관련 service 클래스 생성

---
+++ src/main/java/org/goorm/veri/veribe/domain/book/entity/service/BookshelfService.java
@@ -0,0 +1,21 @@
+package org.goorm.veri.veribe.domain.book.entity.service;
+
+import org.goorm.veri.veribe.domain.book.entity.dtos.memberBook.MemberBookResponse;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface BookshelfService {
+
+    Long addBookshelf(Long memberId, Long bookId, Double score, LocalDateTime startedAt, LocalDateTime endedAt);
+
+    List<MemberBookResponse> searchAll(Long memberId);
+
+//    MemberBookResponse searchDetail(Long memberBookId);
+
+    void readStart(Long memberBookId);
+
+    void readOver(Long memberBookId);
+
+    void deleteBook(Long memberBookId);
+}
