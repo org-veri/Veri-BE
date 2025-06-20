f9e57e0368f439d25756105ba91a32619036c30d
Jinyoung <glitt5384@naver.com>
2025-06-21 04:03:06 +0900

    refactor. 파일 확장자 enum으로 판단

--- src/main/java/org/goorm/veri/veribe/domain/image/service/ImageQueryServiceImpl.java
+++ src/main/java/org/goorm/veri/veribe/domain/image/service/ImageQueryServiceImpl.java
@@ -4,46 +4,37 @@ import org.goorm.veri.veribe.domain.image.exception.DirectoryErrorCode;
 import org.goorm.veri.veribe.domain.image.exception.DirectoryException;
 import org.goorm.veri.veribe.domain.image.exception.ImageErrorCode;
 import org.goorm.veri.veribe.domain.image.exception.ImageException;
+import org.goorm.veri.veribe.domain.image.service.enums.FileExtension;
 import org.goorm.veri.veribe.domain.image.service.enums.FileInfo;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.io.File;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
 
 @Service
-@Transactional(readOnly = true)
 public class ImageQueryServiceImpl implements ImageQueryService{
 
     @Override
     public List<String> fetchUploadedImages(String email) throws ImageException, DirectoryException {
+
+
         File userDir = new File(fetchUserDirectory(email));
 
-        if (!userDir.exists() || !userDir.isDirectory()) {
-            throw new DirectoryException(DirectoryErrorCode.NOT_FOUND);
-        }
-
         File[] imageFiles = userDir.listFiles((dir, name) ->
-                name.toLowerCase().endsWith(".jpg") || name.toLowerCase().endsWith(".jpeg") || name.toLowerCase().endsWith(".png")
+                FileExtension.checkAvailable(name)
         );
 
         if (imageFiles == null || imageFiles.length == 0) {
             return List.of(); // 업로드된 이미지 없음
         }
 
-        return List.of(imageFiles).stream()
-                .map(file -> {
-                    try {
-                        byte[] fileContent = Files.readAllBytes(file.toPath());
-                        return Base64.getEncoder().encodeToString(fileContent);
-                    } catch (IOException e) {
-                        throw new ImageException(ImageErrorCode.ENCODING_FAILED);
-                    }
-                })
+        return Arrays.stream(imageFiles)
+                .map(this::encodeFileToBase64)
                 .toList();
     }
 
@@ -56,6 +47,16 @@ public class ImageQueryServiceImpl implements ImageQueryService{
             return userDir.getAbsolutePath();
         } else {
             throw new DirectoryException(DirectoryErrorCode.NOT_FOUND);
+        }
+    }
+
+
+    private String encodeFileToBase64(File file) {
+        try {
+            byte[] fileContent = Files.readAllBytes(file.toPath());
+            return Base64.getEncoder().encodeToString(fileContent);
+        } catch (IOException e) {
+            throw new ImageException(ImageErrorCode.ENCODING_FAILED);
         }
     }
 }
