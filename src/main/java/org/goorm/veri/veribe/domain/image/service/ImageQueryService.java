14dc8a5ae5f561e6057dd04811219726ab429cb8
Jinyoung <glitt5384@naver.com>
2025-06-21 12:31:04 +0900

    fix. public 제거

--- src/main/java/org/goorm/veri/veribe/domain/image/service/ImageQueryService.java
+++ src/main/java/org/goorm/veri/veribe/domain/image/service/ImageQueryService.java
@@ -3,10 +3,9 @@ package org.goorm.veri.veribe.domain.image.service;
 import org.goorm.veri.veribe.domain.image.exception.DirectoryException;
 import org.goorm.veri.veribe.domain.image.exception.ImageException;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface ImageQueryService {
-    public List<String> fetchUploadedImages(String email) throws ImageException, DirectoryException;
-    public String fetchUserDirectory(String email) throws DirectoryException;
+    List<String> fetchUploadedImages(String email) throws ImageException, DirectoryException;
+    String fetchUserDirectory(String email) throws DirectoryException;
 }
