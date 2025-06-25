3476b270aa42a10ca9883dbbcb424db3d2b57914
Jinyoung <glitt5384@naver.com>
2025-06-25 14:39:22 +0900

    feat. 이미지 엔티티

---
+++ src/main/java/org/goorm/veri/veribe/domain/image/entity/Image.java
@@ -0,0 +1,28 @@
+package org.goorm.veri.veribe.domain.image.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.goorm.veri.veribe.domain.member.entity.Member;
+import org.goorm.veri.veribe.global.entity.BaseEntity;
+
+@Getter
+@Builder
+@Entity
+@Table(name = "image")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Image extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+}
