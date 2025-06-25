c103cd05882ca5216c915bee9621932d4932f483
Jinyoung <glitt5384@naver.com>
2025-06-25 14:42:30 +0900

    feat. imageKey 필드 추가

--- src/main/java/org/goorm/veri/veribe/global/storage/dto/PresignedUrlResponse.java
+++ src/main/java/org/goorm/veri/veribe/global/storage/dto/PresignedUrlResponse.java
@@ -2,6 +2,7 @@ package org.goorm.veri.veribe.global.storage.dto;
 
 public record PresignedUrlResponse(
         String presignedUrl,
-        String publicUrl
+        String publicUrl,
+        String imageKey
 ) {
 }
