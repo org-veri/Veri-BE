9e20677d4bce440a150c003c90ebffa77507e49a
ojy <ojy654@naver.com>
2025-06-27 16:15:03 +0900

    fix. @AuthenticatedMember 사용하여 책장 책 추가 로직, 회원 책장 전체 조회 로직 수정, MemberBookAddRequest DTO 삭제

--- src/main/java/org/goorm/veri/veribe/domain/book/dtos/memberBook/MemberBookAddRequest.java
+++
@@ -1,7 +0,0 @@
-package org.goorm.veri.veribe.domain.book.dtos.memberBook;
-
-import org.goorm.veri.veribe.domain.book.dtos.book.BookRequest;
-
-public record MemberBookAddRequest (
-        Long memberId,
-        BookRequest bookRequest) {}
