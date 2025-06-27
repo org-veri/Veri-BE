ad16036588efd66f2ff6ac658e2b05629f06d99b
Jinyoung <glitt5384@naver.com>
2025-06-27 10:11:20 +0900

    feat. 응답에 페이지네이션 적용

--- src/main/java/org/goorm/veri/veribe/domain/image/controller/ImageController.java
+++ src/main/java/org/goorm/veri/veribe/domain/image/controller/ImageController.java
@@ -1,17 +1,20 @@
 package org.goorm.veri.veribe.domain.image.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.goorm.veri.veribe.domain.auth.annotation.AuthenticatedMember;
+
+import org.goorm.veri.veribe.domain.image.dto.response.PageResponse;
 import org.goorm.veri.veribe.domain.image.service.ImageCommandService;
 import org.goorm.veri.veribe.domain.image.service.ImageQueryService;
-import org.goorm.veri.veribe.domain.member.entity.Member;
+
 import org.namul.api.payload.response.DefaultResponse;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/v0/images")
@@ -29,8 +32,12 @@ public class ImageController {
     }
 
     @GetMapping
-    public DefaultResponse<List<String>> getImageFiles() throws IOException {
+    public DefaultResponse<PageResponse<String>> getImageFiles(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) throws IOException {
         // TODO: 인증 정보 도입
-        return DefaultResponse.ok(imageQueryService.fetchUploadedImages(1L));
+        Pageable pageable = PageRequest.of(page - 1, size); // 백 페이지네이션 시에는 1-based index 를 0으로 보정.
+        return DefaultResponse.ok(imageQueryService.fetchUploadedImages(1L, pageable));
     }
 }
