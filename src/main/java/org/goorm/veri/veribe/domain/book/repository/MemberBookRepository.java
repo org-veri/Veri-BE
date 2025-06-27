c174686e3c987292ab41a181b6fd9dc716316c39
ojy <ojy654@naver.com>
2025-06-27 20:15:51 +0900

    feat. 일주일간 인기있던 책 조회하는 Repository 메서드 추가

--- src/main/java/org/goorm/veri/veribe/domain/book/repository/MemberBookRepository.java
+++ src/main/java/org/goorm/veri/veribe/domain/book/repository/MemberBookRepository.java
@@ -1,10 +1,12 @@
 package org.goorm.veri.veribe.domain.book.repository;
 
 import io.lettuce.core.dynamic.annotation.Param;
+import org.goorm.veri.veribe.domain.book.entity.Book;
 import org.goorm.veri.veribe.domain.book.entity.MemberBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,4 +19,13 @@ public interface MemberBookRepository extends JpaRepository<MemberBook, Long> {
             "WHERE mb.id = :memberBookId")
     Optional<MemberBook> findByIdWithCardsAndBook(@Param("memberBookId") Long memberBookId);
 
+
+    @Query("""
+            SELECT mb.book
+            FROM MemberBook mb
+            WHERE mb.startedAt >= :oneWeekAgo
+            GROUP BY mb.book
+            ORDER BY COUNT(mb) DESC
+            """)
+    List<Book> findMostPopularBook(LocalDateTime oneWeekAgo);
 }
