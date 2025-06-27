c5b5ea2acbc21e6875cab95eaf29726026a8c66d
Jinyoung <glitt5384@naver.com>
2025-06-27 10:12:26 +0900

    feat. 파일 포맷 전달받아 동적으로 처리

--- src/main/java/org/goorm/veri/veribe/global/storage/service/StorageService.java
+++ src/main/java/org/goorm/veri/veribe/global/storage/service/StorageService.java
@@ -14,5 +14,5 @@ public interface StorageService {
                                               String prefix
     );
 
-    void uploadImageToS3(BufferedImage image, String presignedUrl);
+    void uploadImageToS3(BufferedImage image, String presignedUrl, String fileFormat);
 }
