65b4df7ff14ae488af3655a9d07b1a1018cb45eb
ojy <ojy654@naver.com>
2025-06-28 16:27:51 +0900

    fix. MemberBookPagingResponse -> MemberBookListResponse 클래스명 수정, Page 받아 변환하는 로직 추가, converter unused 메서드 삭제

--- src/main/java/org/goorm/veri/veribe/domain/book/dto/memberBook/MemberBookConverter.java
+++ src/main/java/org/goorm/veri/veribe/domain/book/dto/memberBook/MemberBookConverter.java
@@ -2,21 +2,9 @@ package org.goorm.veri.veribe.domain.book.dto.memberBook;
 
 import org.goorm.veri.veribe.domain.book.entity.MemberBook;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MemberBookConverter {
-
-    public static MemberBookResponse toMemberBookResponse(MemberBook memberBook) {
-        return MemberBookResponse.builder()
-                .bookId(memberBook.getBook().getId())
-                .title(memberBook.getBook().getTitle())
-                .imageUrl(memberBook.getBook().getImage())
-                .status(memberBook.getStatus())
-                .author(memberBook.getBook().getAuthor())
-                .score(memberBook.getScore())
-                .build();
-    }
 
     public static MemberBookDetailResponse toMemberBookDetailResponse(MemberBook memberBook) {
 
@@ -33,23 +21,5 @@ public class MemberBookConverter {
                 .author(memberBook.getBook().getAuthor())
                 .cardSummaries(summaries)
                 .build();
-    }
-
-    public static MemberBookPagingResponse toMemberBookSortResponse(List<MemberBook> memberBooks, int page, int size, int totalElements) {
-
-        List<MemberBookResponse> dtoList = new ArrayList<>();
-        for (MemberBook memberBook : memberBooks) {
-            dtoList.add(toMemberBookResponse(memberBook));
-        }
-
-        int totalPages = (int) Math.ceil((double) totalElements / size);
-
-        return new MemberBookPagingResponse(
-                dtoList,
-                page,
-                size,
-                totalElements,
-                totalPages
-        );
     }
 }
