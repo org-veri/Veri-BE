baa4c7b2a008df2ab25e4beeee5cb0cd46a91687
ojy <ojy654@naver.com>
2025-06-28 18:49:15 +0900

    fix. 한주간 인기있는 책 조회를 Pageable 사용하도록 repository 메서드 수정

--- src/main/java/org/goorm/veri/veribe/domain/book/repository/MemberBookRepository.java
+++ src/main/java/org/goorm/veri/veribe/domain/book/repository/MemberBookRepository.java
@@ -1,8 +1,8 @@
 package org.goorm.veri.veribe.domain.book.repository;
 
 import io.lettuce.core.dynamic.annotation.Param;
+import org.goorm.veri.veribe.domain.book.dto.book.BookPopularResponse;
 import org.goorm.veri.veribe.domain.book.dto.memberBook.MemberBookResponse;
-import org.goorm.veri.veribe.domain.book.entity.Book;
 import org.goorm.veri.veribe.domain.book.entity.MemberBook;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,7 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 public interface MemberBookRepository extends JpaRepository<MemberBook, Long> {
@@ -22,13 +21,18 @@ public interface MemberBookRepository extends JpaRepository<MemberBook, Long> {
     Optional<MemberBook> findByIdWithCardsAndBook(@Param("memberBookId") Long memberBookId);
 
     @Query("""
-            SELECT mb.book
+            SELECT new org.goorm.veri.veribe.domain.book.dto.book.BookPopularResponse(
+            mb.book.image,
+            mb.book.title,
+            mb.book.author,
+            mb.book.publisher,
+            mb.book.isbn)
             FROM MemberBook mb
             WHERE mb.startedAt >= :startOfWeek
             GROUP BY mb.book
             ORDER BY COUNT(mb) DESC
             """)
-    List<Book> findMostPopularBook(LocalDateTime startOfWeek);
+    Page<BookPopularResponse> findMostPopularBook(@Param("startOfWeek") LocalDateTime startOfWeek, Pageable pageable);
 
     @Query("""
             SELECT new org.goorm.veri.veribe.domain.book.dto.memberBook.MemberBookResponse(
