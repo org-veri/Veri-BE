a037db298d20dca4f44c4974d588bf50eceb9177
Jinyoung <glitt5384@naver.com>
2025-06-28 16:18:58 +0900

    refactor. upload 함수 제거

--- src/main/java/org/goorm/veri/veribe/global/storage/service/AwsStorageService.java
+++ src/main/java/org/goorm/veri/veribe/global/storage/service/AwsStorageService.java
@@ -84,30 +84,7 @@ public class AwsStorageService implements StorageService {
                 .build()) {
 
             PresignedPutObjectRequest presignedRequest = presigner.presignPutObject(presignRequest);
-            return new PresignedUrlResponse(presignedRequest.url().toString(), getPublicUrl(key), key);
-        }
-    }
-
-    @Override
-    public void uploadImageToS3(BufferedImage image, String imageKey, String fileFormat) {
-        try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
-
-            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                    .bucket(bucket)
-                    .key(imageKey)
-                    .contentType(fileFormat)
-                    .build();
-
-
-            ImageIO.write(image, fileFormat, os);
-            byte[] bytes = os.toByteArray();
-
-
-            s3Client.putObject(putObjectRequest, RequestBody.fromBytes(bytes));
-
-
-        } catch (IOException e) {
-            throw new ImageException(ImageErrorCode.UPLOAD_FAILED);
+            return new PresignedUrlResponse(presignedRequest.url().toString(), getPublicUrl(key));
         }
     }
     private String getPublicUrl(String key) {
