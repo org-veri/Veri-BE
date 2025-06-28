a10a0fe1a9669b9427fdebc931b835d55c3c3134
Jinyoung <glitt5384@naver.com>
2025-06-28 16:22:23 +0900

    fix. imageKey 삭제

--- src/main/java/org/goorm/veri/veribe/global/storage/dto/PresignedUrlResponse.java
+++ src/main/java/org/goorm/veri/veribe/global/storage/dto/PresignedUrlResponse.java
@@ -2,7 +2,6 @@ package org.goorm.veri.veribe.global.storage.dto;
 
 public record PresignedUrlResponse(
         String presignedUrl,
-        String publicUrl,
-        String imageKey
+        String publicUrl
 ) {
 }
