66f6d0837654740f3554f6372841b19085c64dd9
Jeongmo Seo <wjdah1402@gmail.com>
2025-06-30 22:27:54 +0900

    fix. Member Annotation 스웨거에 보이도록 변경

--- src/main/java/org/goorm/veri/veribe/domain/auth/annotation/AuthenticatedMember.java
+++ src/main/java/org/goorm/veri/veribe/domain/auth/annotation/AuthenticatedMember.java
@@ -1,8 +1,11 @@
 package org.goorm.veri.veribe.domain.auth.annotation;
 
+import io.swagger.v3.oas.annotations.Parameter;
+
 import java.lang.annotation.*;
 
 @Documented
+@Parameter(hidden = true)
 @Target({
         ElementType.PARAMETER
 })
