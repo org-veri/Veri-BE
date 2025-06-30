535fdac08eb02d0f47760b415291c358e22d0596
Jeongmo Seo <wjdah1402@gmail.com>
2025-06-30 21:50:44 +0900

    fix. DTO 이름 변경

--- src/main/java/org/goorm/veri/veribe/domain/auth/service/AuthService.java
+++ src/main/java/org/goorm/veri/veribe/domain/auth/service/AuthService.java
@@ -1,9 +1,9 @@
 package org.goorm.veri.veribe.domain.auth.service;
 
-import org.goorm.veri.veribe.domain.auth.dto.OAuth2Request;
-import org.goorm.veri.veribe.domain.auth.dto.OAuth2Response;
+import org.goorm.veri.veribe.domain.auth.dto.AuthRequest;
+import org.goorm.veri.veribe.domain.auth.dto.AuthResponse;
 
 public interface AuthService {
-    OAuth2Response.LoginResponse login(String provider, String code);
-    OAuth2Response.ReissueTokenResponse reissueToken(OAuth2Request.AuthReissueRequest request);
+    AuthResponse.LoginResponse login(String provider, String code);
+    AuthResponse.ReissueTokenResponse reissueToken(AuthRequest.AuthReissueRequest request);
 }
