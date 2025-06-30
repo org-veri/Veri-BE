27249e51719d669e30cf25dcad23210a6c3cb22b
Jeongmo Seo <wjdah1402@gmail.com>
2025-06-30 22:27:24 +0900

    feat. 본인 정보 API 구현

--- src/main/java/org/goorm/veri/veribe/domain/member/service/MemberQueryServiceImpl.java
+++ src/main/java/org/goorm/veri/veribe/domain/member/service/MemberQueryServiceImpl.java
@@ -1,6 +1,10 @@
 package org.goorm.veri.veribe.domain.member.service;
 
 import lombok.RequiredArgsConstructor;
+import org.goorm.veri.veribe.domain.book.repository.MemberBookRepository;
+import org.goorm.veri.veribe.domain.card.repository.CardRepository;
+import org.goorm.veri.veribe.domain.member.converter.MemberConverter;
+import org.goorm.veri.veribe.domain.member.dto.MemberResponse;
 import org.goorm.veri.veribe.domain.member.entity.Member;
 import org.goorm.veri.veribe.domain.member.exception.MemberErrorCode;
 import org.goorm.veri.veribe.domain.member.exception.MemberException;
@@ -12,10 +16,17 @@ import org.springframework.stereotype.Service;
 public class MemberQueryServiceImpl implements MemberQueryService {
 
     private final MemberRepository memberRepository;
+    private final MemberBookRepository memberBookRepository;
+    private final CardRepository cardRepository;
 
     @Override
     public Member findById(Long id) {
         return memberRepository.findById(id).orElseThrow(() ->
                 new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
+    }
+
+    @Override
+    public MemberResponse.MemberInfoResponse findMyInfo(Member member) {
+        return MemberConverter.toMemberInfoResponse(member, memberBookRepository.countAllByMember(member), cardRepository.countAllByMemberId(member.getId()));
     }
 }
