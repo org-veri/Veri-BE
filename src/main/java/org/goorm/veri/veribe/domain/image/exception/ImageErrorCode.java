d4ebcf09b7d7e9c956f665cc89037b48ea704149
Jinyoung <glitt5384@naver.com>
2025-07-01 09:54:22 +0900

    feat. 유효하지 않은 크롭 범위 에러코드 추가

--- src/main/java/org/goorm/veri/veribe/domain/image/exception/ImageErrorCode.java
+++ src/main/java/org/goorm/veri/veribe/domain/image/exception/ImageErrorCode.java
@@ -13,10 +13,9 @@ public enum ImageErrorCode implements BaseErrorCode {
     NOT_FOUND(HttpStatus.NOT_FOUND, "IMG404", "해당 이미지를 찾을 수 없습니다."),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "IMG400", "잘못된 요청입니다. 요청을 확인해주세요."),
     FORBIDDEN(HttpStatus.FORBIDDEN, "IMG401", "해당 이미지에 대한 권한이 없습니다."),
-    ENCODING_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "IMG500_1", "이미지 인코딩에 실패했습니다."),
+    CROP_RANGE_INVALID(HttpStatus.INTERNAL_SERVER_ERROR, "IMG500_1", "크롭된 이미지의 좌표 및 높낮이 값이 유효하지 않습니다."),
     SIZE_EXCEEDED(HttpStatus.INTERNAL_SERVER_ERROR, "IMG500_2", "이미지 크기는 1MB를 초과할 수 없습니다."),
-    UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "IMG500_3", "S3 업로드에 실패했습니다."),
-    UNSUPPORTED_TYPE(HttpStatus.INTERNAL_SERVER_ERROR, "IMG500_4", "지원하지 않는 파일 형식입니다.");
+    UNSUPPORTED_TYPE(HttpStatus.INTERNAL_SERVER_ERROR, "IMG500_3", "지원하지 않는 파일 형식입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
