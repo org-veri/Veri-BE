d751ea9226d8a46830a06c330ad1e507fe59260f
Jinyoung <glitt5384@naver.com>
2025-07-01 09:53:54 +0900

    feat. 크롭된 이미지 OCR 기능 추가, OpenCV 기반 전처리 추가

--- src/main/java/org/goorm/veri/veribe/domain/image/service/ImageCommandService.java
+++ src/main/java/org/goorm/veri/veribe/domain/image/service/ImageCommandService.java
@@ -3,5 +3,5 @@ package org.goorm.veri.veribe.domain.image.service;
 import org.goorm.veri.veribe.domain.member.entity.Member;
 
 public interface ImageCommandService {
-    String processImageOcrAndSave(String imageUrl, Member member) throws Exception;
+    String processImageOcrAndSave(Member member, String imageUrl) throws Exception;
 }
