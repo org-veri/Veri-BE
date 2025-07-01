144da03c11009ce8756aa0445be1c64b46cb31a2
Jinyoung <glitt5384@naver.com>
2025-07-01 09:54:44 +0900

    feat. 멤버 컨트롤러에서 전달

--- src/main/java/org/goorm/veri/veribe/domain/image/service/ImageQueryServiceImpl.java
+++ src/main/java/org/goorm/veri/veribe/domain/image/service/ImageQueryServiceImpl.java
@@ -2,18 +2,12 @@ package org.goorm.veri.veribe.domain.image.service;
 
 import lombok.RequiredArgsConstructor;
 import org.goorm.veri.veribe.domain.image.dto.response.PageResponse;
-import org.goorm.veri.veribe.domain.image.exception.ImageErrorCode;
-import org.goorm.veri.veribe.domain.image.exception.ImageException;
 import org.goorm.veri.veribe.domain.image.repository.ImageRepository;
+import org.goorm.veri.veribe.domain.member.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.Base64;
 import java.util.List;
 
 @Service
@@ -22,8 +16,8 @@ public class ImageQueryServiceImpl implements ImageQueryService{
     private final ImageRepository imageRepository;
 
     @Override
-    public PageResponse<List<String>> fetchUploadedImages(Long userId, Pageable pageable) {
-        Page<String> imageUrls = imageRepository.findByMemberId(userId, pageable);
+    public PageResponse<List<String>> fetchUploadedImages(Member member, Pageable pageable) {
+        Page<String> imageUrls = imageRepository.findByMemberId(member.getId(), pageable);
 
         if(imageUrls.isEmpty()){
             return PageResponse.empty(pageable);
