fbc346620b1d2d49712b66e65cfb4aa1e6b3f37d
Jinyoung <glitt5384@naver.com>
2025-07-01 09:54:44 +0900

    feat. 멤버 컨트롤러에서 전달

--- src/main/java/org/goorm/veri/veribe/domain/image/service/ImageQueryService.java
+++ src/main/java/org/goorm/veri/veribe/domain/image/service/ImageQueryService.java
@@ -1,11 +1,12 @@
 package org.goorm.veri.veribe.domain.image.service;
 
 import org.goorm.veri.veribe.domain.image.dto.response.PageResponse;
+import org.goorm.veri.veribe.domain.member.entity.Member;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 
 public interface ImageQueryService {
-    PageResponse<List<String>> fetchUploadedImages(Long userId, Pageable pageable);
+    PageResponse<List<String>> fetchUploadedImages(Member member, Pageable pageable);
 }
