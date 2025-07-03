5ebdd63b118c3f375b4293b79f8a6e8dcb48ccc7
ojy <ojy654@naver.com>
2025-07-03 16:39:06 +0900

    fix. MemberBook 자세히 조회 시 독서 시작 시간도 출력되도록 변경

--- src/main/java/org/goorm/veri/veribe/domain/book/dto/memberBook/MemberBookDetailResponse.java
+++ src/main/java/org/goorm/veri/veribe/domain/book/dto/memberBook/MemberBookDetailResponse.java
@@ -3,6 +3,7 @@ package org.goorm.veri.veribe.domain.book.dto.memberBook;
 import lombok.Builder;
 import org.goorm.veri.veribe.domain.book.entity.enums.BookStatus;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Builder
@@ -13,5 +14,6 @@ public record MemberBookDetailResponse (
         String imageUrl,
         BookStatus status,
         Double score,
+        LocalDateTime startedAt,
         List<CardSummaries> cardSummaries)
 {}
