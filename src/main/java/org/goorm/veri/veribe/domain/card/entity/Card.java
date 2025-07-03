3ca6561cae55408f96495a3c7f641e0cf8b63518
Soap <miensop51@gmail.com>
2025-07-03 20:57:34 +0900

    fix. 카드 남아있는 상태에서 책 삭제 가능하게 수정

--- src/main/java/org/goorm/veri/veribe/domain/card/entity/Card.java
+++ src/main/java/org/goorm/veri/veribe/domain/card/entity/Card.java
@@ -1,7 +1,21 @@
 package org.goorm.veri.veribe.domain.card.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.goorm.veri.veribe.domain.book.entity.MemberBook;
 import org.goorm.veri.veribe.global.entity.BaseEntity;
 
@@ -25,6 +39,16 @@ public class Card extends BaseEntity {
     private String image;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_book_id")
+    @JoinColumn(
+            name = "member_book_id",
+            foreignKey = @ForeignKey(
+                    name = "fk_card_member_book",
+                    value = ConstraintMode.CONSTRAINT,
+                    foreignKeyDefinition =
+                            "FOREIGN KEY (member_book_id) " +
+                                    "REFERENCES member_book(member_book_id) " +
+                                    "ON DELETE SET NULL"
+            )
+    )
     private MemberBook memberBook;
 }
