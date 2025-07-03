5c664316f4dc28a908dcca7cc44d645f89579aa1
Soap <miensop51@gmail.com>
2025-07-03 19:18:07 +0900

    feat. 카드 응답에 생성 시간 추가

--- src/main/java/org/goorm/veri/veribe/domain/card/controller/dto/CardDetailResponse.java
+++ src/main/java/org/goorm/veri/veribe/domain/card/controller/dto/CardDetailResponse.java
@@ -2,10 +2,13 @@ package org.goorm.veri.veribe.domain.card.controller.dto;
 
 import org.goorm.veri.veribe.domain.book.entity.Book;
 
+import java.time.LocalDateTime;
+
 public record CardDetailResponse(
         Long id,
         String content,
         String imageUrl,
+        LocalDateTime createdAt,
         BookInfo book
 ) {
 
--- src/main/java/org/goorm/veri/veribe/domain/card/repository/dto/CardListItem.java
+++ src/main/java/org/goorm/veri/veribe/domain/card/repository/dto/CardListItem.java
@@ -3,10 +3,13 @@ package org.goorm.veri.veribe.domain.card.repository.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @AllArgsConstructor
 public class CardListItem {
     private Long cardId;
     private String content;
     private String image;
+    private LocalDateTime created;
 }
