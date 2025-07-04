d4165001df9e3591dc568a40c8e933fbd37dbef0
ojy <ojy654@naver.com>
2025-07-04 20:17:07 +0900

    fix. MemberBookScoreRequest 에서 별점을 .5단위로 받을 수 있도록 수정

--- src/main/java/org/goorm/veri/veribe/domain/book/dto/memberBook/MemberBookScoreRequest.java
+++ src/main/java/org/goorm/veri/veribe/domain/book/dto/memberBook/MemberBookScoreRequest.java
@@ -10,12 +10,10 @@ import java.math.BigDecimal;
 public record MemberBookScoreRequest (
         @Min(0)
         @Max(5) //최소 0점 ~ 최고 5점
-        @Digits(integer = 1, fraction = 0) //소수점 단위 수는 받을 수 없도록 제한
+        @Digits(integer = 1, fraction = 1)
         Double score
 ) {
 
-    //추후 기획 변경을 통해 0.5 단위로 점수 받을 시, 활성화 & fraction = 1 로 수정
-    /*
     @AssertTrue(message = "0.5 단위로만 입력할 수 있습니다.")
     private boolean isHalfStep() {
         if (score == null) return true;
@@ -25,5 +23,4 @@ public record MemberBookScoreRequest (
 
         return remainder.compareTo(BigDecimal.ZERO) == 0; //나머지가 0 이면 .5 단위 수 -> 정상
     }
-    */
 }
