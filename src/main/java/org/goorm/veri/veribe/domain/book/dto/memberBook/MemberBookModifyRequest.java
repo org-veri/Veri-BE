6abb3a0b6c21c9139d4b771bf5b364d4f6da59e8
ojy <ojy654@naver.com>
2025-07-05 19:51:30 +0900

    feat. MemberBook 전체 수정을 위한 MemberBookModifyRequest DTO 추가

--- src/main/java/org/goorm/veri/veribe/domain/book/dto/memberBook/MemberBookModifyRequest.java
+++ src/main/java/org/goorm/veri/veribe/domain/book/dto/memberBook/MemberBookModifyRequest.java
@@ -10,7 +10,13 @@ public record MemberBookModifyRequest(
         @Max(5)
         @Digits(integer = 1, fraction = 1)
         Double score,
+        //독서 시작 시간, 완료 시간 수정은 무조건 과거 시간으로 받고,
+        //null값 허용을 제한하여 변경하지 않을 시엔 기존값을 넘겨받도록 하기
+        @Past
+        @NotNull
         LocalDateTime startedAt,
+        @Past
+        @NotNull
         LocalDateTime endedAt
 ) {
     @AssertTrue(message = "0.5 단위로만 입력할 수 있습니다.")
