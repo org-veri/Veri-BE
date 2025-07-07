ad0f54114d804d6ade0e5682570cbc1c55c4391a
Soap <miensop51@gmail.com>
2025-07-07 17:49:43 +0900

    chore. 개발환경 설정 : redis 의존성 제거

--- src/main/java/org/goorm/veri/veribe/global/config/RedisConfig.java
+++ src/main/java/org/goorm/veri/veribe/global/config/RedisConfig.java
@@ -8,7 +8,9 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.context.annotation.Profile;
 
+@Profile("!dev")
 @Configuration
 @RequiredArgsConstructor
 public class RedisConfig {
