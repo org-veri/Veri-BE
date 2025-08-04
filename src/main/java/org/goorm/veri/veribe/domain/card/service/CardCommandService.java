b003367ef97cd0ce43f4f04d4fd26dfc692a0e74
Soap <miensop51@gmail.com>
2025-08-04 22:42:27 +0900

    refactor. 불필요한 카드 서비스 인터페이스 제거

--- src/main/java/org/goorm/veri/veribe/domain/card/service/CardCommandService.java
+++ src/main/java/org/goorm/veri/veribe/domain/card/service/CardCommandService.java
@@ -1,17 +1,94 @@
 package org.goorm.veri.veribe.domain.card.service;
 
 import io.github.miensoap.s3.core.post.dto.PresignedPostForm;
+import lombok.RequiredArgsConstructor;
+import org.goorm.veri.veribe.domain.book.entity.MemberBook;
+import org.goorm.veri.veribe.domain.book.repository.MemberBookRepository;
+import org.goorm.veri.veribe.domain.card.entity.Card;
+import org.goorm.veri.veribe.domain.card.entity.CardContentText;
+import org.goorm.veri.veribe.domain.card.exception.CardErrorInfo;
+import org.goorm.veri.veribe.domain.card.repository.CardRepository;
 import org.goorm.veri.veribe.domain.member.entity.Member;
+import org.goorm.veri.veribe.global.exception.http.BadRequestException;
+import org.goorm.veri.veribe.global.exception.http.ForbiddenException;
+import org.goorm.veri.veribe.global.exception.http.NotFoundException;
 import org.goorm.veri.veribe.global.storage.dto.PresignedUrlRequest;
 import org.goorm.veri.veribe.global.storage.dto.PresignedUrlResponse;
+import org.goorm.veri.veribe.global.storage.service.StorageService;
+import org.goorm.veri.veribe.global.storage.service.StorageUtil;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface CardCommandService {
+import java.time.Duration;
 
-    Long createCard(Member member, String content, String imageUrl, Long memberBookId);
+import static org.goorm.veri.veribe.global.storage.service.StorageConstants.MB;
 
-    void deleteCard(Long memberId, Long cardId);
+@Service
+@RequiredArgsConstructor
+public class CardCommandService {
 
-    PresignedUrlResponse getPresignedUrl(PresignedUrlRequest request);
+    private final CardRepository cardRepository;
+    private final MemberBookRepository memberBookRepository;
+    private final StorageService storageService;
 
-    PresignedPostForm getPresignedPost();
+    @Transactional
+    public Long createCard(Member member, CardContentText content, String imageUrl, Long memberBookId) {
+        MemberBook memberBook = memberBookRepository.findById(memberBookId)
+                .orElseThrow(() -> new BadRequestException(CardErrorInfo.BAD_REQUEST));
+
+        Card card = Card.builder()
+                .member(member)
+                .content(content)
+                .image(imageUrl)
+                .memberBook(memberBook)
+                .build();
+
+        cardRepository.save(card);
+        return card.getId();
+    }
+
+    @Transactional
+    public void deleteCard(Long memberId, Long cardId) {
+        Card card = cardRepository.findById(cardId)
+                .orElseThrow(() -> new NotFoundException(CardErrorInfo.NOT_FOUND));
+
+        if (!card.getMember().getId().equals(memberId)) {
+            throw new ForbiddenException(CardErrorInfo.FORBIDDEN);
+        }
+
+        cardRepository.deleteById(cardId);
+    }
+
+    public PresignedUrlResponse getPresignedUrl(PresignedUrlRequest request) {
+        int expirationMinutes = 5;
+        String prefix = "public";
+
+        if (request.contentLength() > MB) {
+            throw new BadRequestException(CardErrorInfo.IMAGE_TOO_LARGE);
+        }
+
+        if (!StorageUtil.isImage(request.contentType()))
+            throw new BadRequestException(CardErrorInfo.UNSUPPORTED_IMAGE_TYPE);
+
+        return storageService.generatePresignedUrl(
+                request.contentType(),
+                request.contentLength(),
+                prefix,
+                Duration.ofMinutes(expirationMinutes)
+        );
+    }
+
+    public PresignedPostForm getPresignedPost() {
+        String allowedContentType = "image/*";
+        int expirationMinutes = 5;
+        long allowedSize = MB; // 1MB
+        String prefix = "public";
+
+        return storageService.generatePresignedPost(
+                allowedContentType,
+                allowedSize,
+                prefix,
+                Duration.ofMinutes(expirationMinutes)
+        );
+    }
 }
--- src/main/java/org/goorm/veri/veribe/domain/card/service/CardQueryService.java
+++ src/main/java/org/goorm/veri/veribe/domain/card/service/CardQueryService.java
@@ -1,15 +1,38 @@
 package org.goorm.veri.veribe.domain.card.service;
 
+import lombok.RequiredArgsConstructor;
 import org.goorm.veri.veribe.domain.card.controller.enums.CardSortType;
 import org.goorm.veri.veribe.domain.card.entity.Card;
+import org.goorm.veri.veribe.domain.card.exception.CardErrorInfo;
+import org.goorm.veri.veribe.domain.card.repository.CardRepository;
 import org.goorm.veri.veribe.domain.card.repository.dto.CardListItem;
+import org.goorm.veri.veribe.global.exception.http.NotFoundException;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface CardQueryService {
+@Transactional(readOnly = true)
+@Service
+@RequiredArgsConstructor
+public class CardQueryService {
 
-    Page<CardListItem> getOwnedCards(Long memberId, int page, int size, CardSortType sortType);
+    private final CardRepository cardRepository;
 
-    Card getCardById(Long cardId);
+    public Page<CardListItem> getOwnedCards(Long memberId, int page, int size, CardSortType sortType) {
+        Pageable pageRequest = PageRequest.of(page, size, sortType.getSort());
 
-    int getOwnedCardCount(Long memberId);
+        return cardRepository.findAllByMemberId(memberId, pageRequest);
+    }
+
+    public Card getCardById(Long cardId) {
+        System.out.println(cardRepository.findById(cardId).get().getContent());
+        return cardRepository.findById(cardId)
+                .orElseThrow(() -> new NotFoundException(CardErrorInfo.NOT_FOUND));
+    }
+
+    public int getOwnedCardCount(Long memberId) {
+        return cardRepository.countAllByMemberId(memberId);
+    }
 }
