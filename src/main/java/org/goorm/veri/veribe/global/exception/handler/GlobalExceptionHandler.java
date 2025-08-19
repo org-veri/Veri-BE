2d69262a3b83e194dbd3ebbd5fa0aab5c36d72c8
Soap <miensop51@gmail.com>
2025-08-20 00:17:39 +0900

    chore. 로그 레벨 조정, mock 데이터

--- src/main/java/org/goorm/veri/veribe/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/org/goorm/veri/veribe/global/exception/handler/GlobalExceptionHandler.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
@@ -27,9 +28,9 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ApplicationException.class)
     public ApiResponse<Map<?, ?>> handleApplicationException(ApplicationException e) {
         if (e.getHttpStatus().is4xxClientError()) {
-            log.warn(e.getMessage());
+            log.info(e.getMessage());
         } else {
-            log.error(e.getMessage(), e);
+            log.warn(e.getMessage(), e);
         }
 
         return ApiResponse.error(e.getErrorInfo(), e.getHttpStatus());
@@ -37,43 +38,44 @@ public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public ApiResponse<Map<?, ?>> handleAnyCheckedException(Exception e) {
-        log.error("Unexpected Error Occurred", e);
+    public ApiResponse<Map<?, ?>> handleAnyUnexpectedException(Exception e) {
+        log.warn("Unexpected Error Occurred", e);
         return ApiResponse.error(CommonErrorInfo.INTERNAL_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<Map<?, ?>>> handleMethodArgumentNotValidException(
-            MethodArgumentNotValidException e) {
-        log.warn(e.getMessage());
-        return ApiResponse.validationFailure(e.getFieldErrors());
-    }
-
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoResourceFoundException.class)
     public ApiResponse<Map<?, ?>> handleNoResourceFoundException(NoResourceFoundException e,
                                                                  HttpServletRequest request) {
-        log.warn("Resource '{}' Not Found. Request IP : {}", request.getRequestURI(),
+        log.debug("Resource '{}' Not Found. Request IP : {}", request.getRequestURI(),
                 request.getAttribute(InjectIPAddressInterceptor.IP));
         return ApiResponse.error(CommonErrorInfo.RESOURCE_NOT_FOUND, HttpStatus.NOT_FOUND);
     }
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ApiResponse<Map<?, ?>> handleHttpRequestNotSupportedException(
             HttpRequestMethodNotSupportedException e,
             HttpServletRequest request) {
-        log.warn("Method {} for '{}' Not supported. Request IP : {}", request.getMethod(),
+        log.debug("Method {} for '{}' Not supported. Request IP : {}", request.getMethod(),
                 request.getRequestURI(),
                 request.getAttribute(InjectIPAddressInterceptor.IP));
         return ApiResponse.error(CommonErrorInfo.RESOURCE_NOT_FOUND, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(UnrecognizedPropertyException.class)
-    public ApiResponse<Map<?, ?>> handleUnrecognizedPropertyException(
-            UnrecognizedPropertyException e,
-            HttpServletRequest request
-    ) {
-        log.error("Unrecognized property in request for {} : {}", request.getRequestURI(),
-                e.getMessage());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Map<?, ?>>> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        log.debug(e.getMessage());
+        return ApiResponse.validationFailure(e.getFieldErrors());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ApiResponse<Map<?, ?>> handleMethodArgumentTypeMismatchException(
+            MethodArgumentTypeMismatchException e) {
+        log.debug(e.getMessage());
         return ApiResponse.error(CommonErrorInfo.INVALID_REQUEST, HttpStatus.BAD_REQUEST);
     }
 
@@ -81,15 +83,25 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ApiResponse<Map<?, ?>> handleHttpMessageNotReadableException(
             HttpMessageNotReadableException e) {
-        log.warn(e.getMessage());
+        log.debug(e.getMessage());
         return ApiResponse.error(CommonErrorInfo.INVALID_REQUEST, HttpStatus.BAD_REQUEST);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ApiResponse<Map<?, ?>> handleMethodArgumentTypeMismatchException(
-            MethodArgumentTypeMismatchException e) {
-        log.warn(e.getMessage());
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ApiResponse<Map<?, ?>> handleHandlerMethodValidationException(
+            HandlerMethodValidationException e) {
+        log.debug(e.getMessage());
+        return ApiResponse.error(CommonErrorInfo.INVALID_REQUEST, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UnrecognizedPropertyException.class)
+    public ApiResponse<Map<?, ?>> handleUnrecognizedPropertyException(
+            UnrecognizedPropertyException e,
+            HttpServletRequest request
+    ) {
+        log.debug("Unrecognized property in request for {} : {}", request.getRequestURI(),
+                e.getMessage());
         return ApiResponse.error(CommonErrorInfo.INVALID_REQUEST, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/org/goorm/veri/veribe/mock/InsertMockData.java
+++ src/main/java/org/goorm/veri/veribe/mock/InsertMockData.java
@@ -2,6 +2,9 @@ package org.goorm.veri.veribe.mock;
 
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.goorm.veri.veribe.domain.book.entity.Book;
+import org.goorm.veri.veribe.domain.book.entity.MemberBook;
+import org.goorm.veri.veribe.domain.card.entity.Card;
 import org.goorm.veri.veribe.domain.member.entity.Member;
 import org.goorm.veri.veribe.domain.member.entity.enums.ProviderType;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -27,37 +30,49 @@ public class InsertMockData {
         Member member = Member.builder()
                 .email("test@test.com")
                 .nickname("테스트")
-                .image("https://example.com/image.jpg")
+                .profileImageUrl("https://example.com/image.jpg")
                 .providerId("test-provider-id")
                 .providerType(ProviderType.KAKAO)
                 .build();
 
         em.persist(member);
 
-//        Book book = Book.builder()
-//                .title("테스트 책")
-//                .image("https://example.com/book-image.jpg")
-//                .author("<UNK> <UNK>")
-//                .publisher("member")
-//                .isbn("123456789")
-//                .build();
-//
-//        em.persist(book);
-//
-//        MemberBook memberBook = MemberBook.builder()
-//                .member(member)
-//                .book(book)
-//                .build();
-//
-//        em.persist(memberBook);
-//
-//        for (int i = 0; i < 10; i++) {
-//            Card card = Card.builder()
-//                    .image("https://example.com/card-image-" + i + ".jpg")
-//                    .content("테스트 카드 내용 " + i)
-//                    .memberBook(memberBook)
-//                    .build();
-//            em.persist(card);
-//        }
+        Book book = Book.builder()
+                .title("테스트 책")
+                .image("https://example.com/book-image.jpg")
+                .author("<UNK> <UNK>")
+                .publisher("member")
+                .isbn("123456789")
+                .build();
+
+        em.persist(book);
+
+        MemberBook memberBook = MemberBook.builder()
+                .member(member)
+                .book(book)
+                .build();
+
+        em.persist(memberBook);
+
+        for (int i = 0; i < 5; i++) {
+            Card card = Card.builder()
+                    .image("https://example.com/card-image-" + i + ".jpg")
+                    .content("테스트 카드 내용 " + i)
+                    .memberBook(memberBook)
+                    .member(member)
+                    .build();
+            em.persist(card);
+        }
+
+        for (int i = 5; i < 10; i++) {
+            Card card = Card.builder()
+                    .image("https://example.com/card-image-" + i + ".jpg")
+                    .content("테스트 카드 내용 " + i)
+                    .memberBook(memberBook)
+                    .member(member)
+                    .isPublic(true)
+                    .build();
+            em.persist(card);
+        }
     }
 }
