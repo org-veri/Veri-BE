3ee767d750673027d1cd7fe8ecf940ee768782ca
soap <miensop51@gmail.com>
2025-11-09 15:14:30 +0900

    refactor. config 클래스 정리

--- src/main/java/org/veri/be/global/config/InterceptorConfig.java
+++ src/main/java/org/veri/be/global/config/InterceptorConfig.java
@@ -1,12 +1,40 @@
 package org.veri.be.global.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.veri.be.domain.auth.service.TokenStorageService;
+import org.veri.be.domain.member.service.MemberQueryService;
+import org.veri.be.global.auth.context.AuthenticatedMemberResolver;
 import org.veri.be.global.interceptors.InjectIPAddressInterceptor;
+import org.veri.be.lib.auth.jwt.JwtFilter;
+
+import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class InterceptorConfig implements WebMvcConfigurer {
+
+    private final AuthenticatedMemberResolver authenticatedMemberResolver;
+    private final MemberQueryService memberQueryService;
+    private final TokenStorageService tokenStorageService;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authenticatedMemberResolver);
+    }
+
+    @Bean
+    public FilterRegistrationBean<OncePerRequestFilter> firstFilterRegister() {
+        return new FilterRegistrationBean<>(new JwtFilter(
+                memberQueryService, tokenStorageService
+        ));
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
