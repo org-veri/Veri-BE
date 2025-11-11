0be7a623078ff462b8cc7c9b2d276cdd21b016a4
soap <miensop51@gmail.com>
2025-11-11 17:55:03 +0900

    feat. webflux 제거

--- src/main/java/org/veri/be/global/config/WebConfig.java
+++ src/main/java/org/veri/be/global/config/WebConfig.java
@@ -1,18 +1,9 @@
 package org.veri.be.global.config;
 
-import io.netty.channel.ChannelOption;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.util.DefaultUriBuilderFactory;
-import reactor.netty.http.client.HttpClient;
-import reactor.netty.resources.ConnectionProvider;
-
-import java.time.Duration;
 
 @Configuration
 @RequiredArgsConstructor
@@ -26,30 +17,5 @@ public class WebConfig implements WebMvcConfigurer {
                         "classpath:/docs/"
                 )
                 .setCachePeriod(3600);
-    }
-
-    private final DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory();
-
-    private final HttpClient httpClient = HttpClient.create()
-            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000); // 10초
-
-    @Bean
-    public WebClient webClient() {
-        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
-        return WebClient.builder()
-                .uriBuilderFactory(factory)
-                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(2 * 1024 * 1024))
-                .clientConnector(new ReactorClientHttpConnector(httpClient))
-                .build();
-    }
-
-    @Bean
-    public ConnectionProvider connectionProvider() {
-        return ConnectionProvider.builder("http-pool")
-                .maxConnections(100)
-                .pendingAcquireTimeout(Duration.ofMillis(0))
-                .pendingAcquireMaxCount(-1)
-                .maxIdleTime(Duration.ofMillis(1000L))
-                .build();
     }
 }
