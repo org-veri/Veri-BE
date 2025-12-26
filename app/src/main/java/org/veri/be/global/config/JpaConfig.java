79429c381d0088cbb3a2c3405d6c4e6fd158af85
soap <miensop51@gmail.com>
2025-12-26 16:07:49 +0900

    refactor. jpa config 분리

---
+++ app/src/main/java/org/veri/be/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package org.veri.be.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
