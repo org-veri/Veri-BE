d227b68a6c8895b81002ce482568b0231e5d9ccf
soap <miensop51@gmail.com>
2025-12-27 13:05:02 +0900

    refactor. add aws client module

---
+++ clients/client-aws/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    `java-library`
+    id("io.spring.dependency-management")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+
+    api("io.github.miensoap:aws-s3:1.0.3")
+    implementation(platform("software.amazon.awssdk:bom:2.25.1"))
+    implementation("software.amazon.awssdk:s3")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+}
--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     testImplementation(project(mapOf("path" to ":core:core-api", "configuration" to "runtimeElements")))
     testImplementation(project(":core:core-enum"))
     testImplementation(project(":clients:client-ocr"))
+    testImplementation(project(":clients:client-aws"))
     testImplementation(project(":clients:client-search"))
     testImplementation(project(":storage:db-core"))
     testImplementation(project(":support:common"))
