ced28decffd1a6d28b5c0c6328c60ae21b8c71e2
soap <miensop51@gmail.com>
2025-12-30 21:20:40 +0900

    chore. add modulith deps to core api

--- core/core-api/build.gradle.kts
+++ core/core-api/build.gradle.kts
@@ -9,6 +9,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-restclient")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation(platform("org.springframework.modulith:spring-modulith-bom:2.0.1"))
+    implementation("org.springframework.modulith:spring-modulith-starter-core")
 
     // JWT
     implementation("io.jsonwebtoken:jjwt-api:0.13.0")
